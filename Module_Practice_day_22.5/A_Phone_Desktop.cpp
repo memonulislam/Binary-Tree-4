44a0a3b47359ca3475eadaac0543be0302098cb9
Memonul Islam Tuhin <memonulislam@gmail.com>
2025-08-07 20:02:25 +0600

    Update A_Phone_Desktop.cpp
    This code is 2nd time code ..

--- Module_Practice_day_22.5/A_Phone_Desktop.cpp
+++ Module_Practice_day_22.5/A_Phone_Desktop.cpp
@@ -30,3 +30,39 @@ int main()
 
     return 0;
 }
+
+-----------------------------------------------------OR--------------------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        int screen = y / 2;
+        int cell = screen * 7;
+        if (y % 2 != 0)
+        {
+            screen++;
+            cell += 11;
+        }
+
+        if (x > cell)
+        {
+            x -= cell;
+            screen += x / 15;
+            if (x % 15 != 0)
+            {
+                screen++;
+            }
+        }
+
+        cout << screen << endl;
+    }
+    return 0;
+}
